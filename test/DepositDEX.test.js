2c0c6a135f21f15e88a6e35f5e876e234cd67e52
Gleb Zykov <user1@Glebs-MacBook-Pro.local>
2024-11-25 19:07:04 +0700

    deposit dex withdraw test with sign typed data

--- test/DepositDEX.test.js
+++ test/DepositDEX.test.js
@@ -115,7 +115,6 @@ describe('DepositDex contract', function () {
         orderValidationWrapper = await OrderValidationWrapper.deploy();
         await orderValidationWrapper.waitForDeployment();
 
-        const withdrawalOrderTypeHash = await orderValidationWrapper.WITHDRAWAL_ORDER_TYPEHASH();
 
         const amount = ethers.parseEther('100');
         await token.mint(alice.address, amount);
@@ -126,88 +125,37 @@ describe('DepositDex contract', function () {
         const withdrawalAmount = ethers.parseEther('10');
         const expiration = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
 
-
         const withdrawalOrder = {
             collateral: await token.getAddress(),
             account: alice.address,
             amount: withdrawalAmount,
             session: ethers.ZeroAddress,
             expiration: expiration,
-            signature: '0x'
         };
 
-        const withdrawalOrderTypeValueHash = ethers.keccak256(
-            ethers.AbiCoder.defaultAbiCoder().encode(
-                ["bytes32", "address", "uint256", "address", "uint256"],
-                [
-                    withdrawalOrderTypeHash,
-                    withdrawalOrder.account,
-                    withdrawalOrder.amount,
-                    withdrawalOrder.session,
-                    withdrawalOrder.expiration,
-                ]
-            )
-        )
+        const domain = {
+            name: "EventHorizon",
+            version: "1",
+            chainId: (await ethers.provider.getNetwork()).chainId,
+            verifyingContract: await depositDex.getAddress()
+        }
 
-        const EIP712_DOMAIN_TYPEHASH = ethers.keccak256(
-            ethers.toUtf8Bytes("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")
-        );
-
-        const name = "EventHorizon"; // Matches `HASHED_NAME` in the contract
-        const version = "1"; // Matches `HASHED_VERSION` in the contract
-        const chainId = (await ethers.provider.getNetwork()).chainId; // Current chain ID
-        const verifyingContract = await depositDex.getAddress(); // Address of the contract (replace with actual contract address)
-
-        const HASHED_NAME = ethers.keccak256(ethers.toUtf8Bytes(name));
-        const HASHED_VERSION = ethers.keccak256(ethers.toUtf8Bytes(version));
-
-        // Encode and hash the domain separator
-        const domainSeparator = ethers.keccak256(
-            ethers.AbiCoder.defaultAbiCoder().encode(
-                ["bytes32", "bytes32", "bytes32", "uint256", "address"],
-                [EIP712_DOMAIN_TYPEHASH, HASHED_NAME, HASHED_VERSION, chainId, verifyingContract]
-            )
-        );
-
-
-        const digest = ethers.keccak256(
-            ethers.concat([
-                ethers.toUtf8Bytes("\x19\x01"), // EIP-712 prefix
-                ethers.getBytes(domainSeparator), // Domain separator
-                ethers.getBytes(withdrawalOrderTypeValueHash), // Hash of the struct
-            ]))
-        const digestHash = ethers.hashMessage(ethers.getBytes(digest))
-
-        const signature = await alice.signMessage(ethers.getBytes(digest));
-        const valid = await orderValidationWrapper.isSignatureValid(alice.address, digestHash, signature);
-
-        console.log('\n\n\n\nvalid:', valid)
-
-        const signedWithdrawalOrder = { ...withdrawalOrder, signature };
-
-        console.log(`alice address ${alice.address}`)
-        console.log(`digest        ${digest}`)
-        console.log(`signature     ${signature}`)
-
+        const types = {
+            OrderWithdrawal: [
+                { name: "account", type: "address" },
+                { name: "amount", type: "uint256" },
+                { name: "session", type: "address" },
+                { name: "expiration", type: "uint256" },
+            ],
+        };
+        
+        const signatureEip712 = await alice.signTypedData(domain, types, withdrawalOrder);
         await depositDex.connect(matcher).withdrawComplete(
-            signedWithdrawalOrder,
+            {...withdrawalOrder, signature: signatureEip712},
             [100000000], // fullPrices
             0, // historyTimestamp
             0 // historySearchHint
         )
-
-        // await expect(
-        //     depositDex.connect(matcher).withdrawComplete(
-        //         signedWithdrawalOrder,
-        //         [100000000], // fullPrices
-        //         0, // historyTimestamp
-        //         0 // historySearchHint
-        //     )
-        // ).to.emit(depositDex, 'DepositBalanceChanged')
-        //     .withArgs(alice.address, await token.getAddress(), -withdrawalAmount, ethers.parseEther('90'));
-
-
-
 
     });
 
